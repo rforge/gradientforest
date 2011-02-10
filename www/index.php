ad787efc83808074bac4709308b97f82b8e14a98
nicke <nicke@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-02-10 01:13:10 +0000

    Update web site with both vignettes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gradientforest@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -41,11 +41,13 @@ while (!feof($handle)) {
 fclose($handle);
 echo $contents; } ?>
 
+
+
 <!-- end of project description -->
 
-<p> No content added. </p>
-
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+<p> The overview vignette for <strong>extendedForest</strong> is <a href="Conditional-importance.pdf"><strong>here</strong></a>. </p>
+<p> The overview vignette for <strong>gradientForest</strong> is <a href="biodiversity-survey.pdf"><strong>here</strong></a>. </p>
+<p> The <strong>project summary page</strong> is <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
 </body>
 </html>
